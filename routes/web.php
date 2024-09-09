4caef3adf639fdcb9dda0ab4b6927c4e6c5d6550
Luis Daniel G. Garcia Herrera <danielrulesu4ss10@gmail.com>
2024-09-09 14:28:08 -0600

    Nuevo Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FichaControlador;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**Rutas CRUD Fichas Tecnicas */
+Route::resource('/ficha', FichaControlador::class);
+/**Rutas CRUD Fichas Tecnicas */
